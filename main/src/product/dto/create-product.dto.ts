60e8bfd034fcc82996a2ca2eee52998e054122cb
mahmoudnabil133 <hodanabil144@gmail.com>
2024-11-05 19:44:34 +0200

    remove id from dto

--- main/src/product/dto/create-product.dto.ts
+++ main/src/product/dto/create-product.dto.ts
@@ -1,9 +1,6 @@
 import { IsInt, IsNotEmpty, IsString } from "class-validator";
 
 export class CreateProductDto {
-    @IsInt()
-    id: number;
-    
     @IsString()
     @IsNotEmpty()
     title: string;
