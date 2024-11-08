8c4816e37bac736e00c0b0c83cdd2468272b425e
mahmoudnabil133 <hodanabil144@gmail.com>
2024-11-08 21:35:14 +0200

    containerize apps

--- admin/src/app.module.ts
+++ admin/src/app.module.ts
@@ -7,16 +7,25 @@ import { Product } from './product/product.entity';
 
 @Module({
   imports: [ProductModule,
-    TypeOrmModule.forRoot({
-      type:'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password:'3mod4od123321',
-      database: 'Microservice',
+    // TypeOrmModule.forRoot({
+    //   type:'mysql',
+    //   host: 'localhost',
+    //   port: 3306,
+    //   username: 'root',
+    //   password:'3mod4od123321',
+    //   database: 'Microservice',
+    //   entities: [Product],
+    //   synchronize: true
+    // }),
+      TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mysql_db',
+      port: 3307,
+      database: 'nest',
       entities: [Product],
-      synchronize: true
-      
+      username: 'testuser',
+      password: 'testuser123',
+      synchronize: true,
     }),
   ],
   controllers: [AppController],
